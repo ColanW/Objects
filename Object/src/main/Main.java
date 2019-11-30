e377b35ecc289a1e63292ceb9d2783ea562d202f
ColanW <54151642+ColanW@users.noreply.github.com>
2019-11-30 04:11:32 -0600

    Add files via upload

---
+++ Object/src/main/Main.java
@@ -0,0 +1,13 @@
+package main;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Object o = new Object("Colan");
+		
+		System.out.println(o.getName());
+		
+	}
+
+}
---
+++ Object/src/main/Object.java
@@ -0,0 +1,29 @@
+package main;
+
+public class Object {
+
+	private String name;
+	
+	public Object() {
+		
+		//Default Constructor
+		
+	}
+	
+	public Object(String name) {
+		
+		this.name = name;
+		
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+	
+}
